8257ef36257b1b90e6b48a1df1d79704d84c2294
Lester A Leong <38544450+LesterALeong@users.noreply.github.com>
2023-05-18 10:54:48 -0500

    Legend displays final ROI values

--- StreamlitMCsim.py
+++ StreamlitMCsim.py
@@ -42,14 +42,26 @@ if st.button('Run Simulations'):
     # Run the simulation with the user inputs
     all_results = investment_simulator(starting_capital, win_percent, win_amount, loss_amount, num_trades, num_simulations)
 
+    # Calculate final values and ROIs
+    final_values = [results[-1] for results in all_results]
+    rois = [(final - starting_capital) * 100.0 / starting_capital for final in final_values]
+
+    # Calculate and display summary statistics
+    avg_roi = np.mean(rois)
+    top_5_roi = np.percentile(rois, 95)
+    bottom_5_roi = np.percentile(rois, 5)
+    st.write(f'Average ROI: {avg_roi:.2f}%')
+    st.write(f'Top 5% ROI: {top_5_roi:.2f}%')
+    st.write(f'Bottom 5% ROI: {bottom_5_roi:.2f}%')
+
     # Plot the results of each simulation
     st.set_option('deprecation.showPyplotGlobalUse', False)
     plt.figure(figsize=(10, 6))
     for i, results in enumerate(all_results):
-        plt.plot(results, label=f'Simulation {i+1}')
+        plt.plot(results)
 
     plt.title('Monte Carlo Simulation of Investment Strategy')
     plt.xlabel('Trade Number')
     plt.ylabel('Portfolio Value')
-    plt.legend(loc='upper left')
+    plt.legend([f'Average ROI: {avg_roi:.2f}%', f'Top 5% ROI: {top_5_roi:.2f}%', f'Bottom 5% ROI: {bottom_5_roi:.2f}%'], loc='upper left')
     st.pyplot()
